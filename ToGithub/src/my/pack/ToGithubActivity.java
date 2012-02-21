1b147f7605c08d9fec346d186c2b380f0a5a258c
akash_malik <akash_malik@MYSHEC222719D.ad.infosys.com>
2012-02-21 16:20:04 +0530

    akhil says hi

---
+++ ToGithub/src/my/pack/ToGithubActivity.java
@@ -0,0 +1,13 @@
+package my.pack;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ToGithubActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
